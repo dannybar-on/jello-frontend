92e77deb7dbc85eabe448659d81f27898c1b72e1
BarakLB <Baraktv@gmail.com>
2022-01-28 13:52:31 +0200

    users modal fix

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { GoogleLogin } from 'react-google-login';
 
-import LoginSvgLeft from '../assets/img/login-svg-left.svg'
-import LoginSvgRight from '../assets/img/login-svg-right.svg'
+import { signup, login, logout } from '../store/user.action.js';
+
+import LoginSvgLeft from '../assets/img/login-svg-left.svg';
+import LoginSvgRight from '../assets/img/login-svg-right.svg';
 import jello from '../assets/img/jello.svg';
 
 
@@ -24,22 +27,20 @@ class _LoginPage extends React.Component {
     onSubmit = (ev) => {
         ev.preventDefault();
         const { username, password, fullname, isLogin } = this.state;
-
         if (isLogin) {
-            const { onLoadUser } = this.props;
+            const { login } = this.props;
             if (!username && !password) return;
             const credentials = {
                 username,
                 password,
             };
 
-            onLoadUser(credentials)
-                .then(() => {
-                    this.props.history.push('/');
-                });
+            console.log('in submit');
+            login(credentials)
+                .then(() => this.props.history.push('/board'));
         } else {
 
-            const { onSignUp } = this.props;
+            const { signup } = this.props;
             if (!username && !password && !fullname) return;
 
             const userInfo = {
@@ -47,8 +48,8 @@ class _LoginPage extends React.Component {
                 password,
                 fullname
             };
-            onSignUp(userInfo)
-                .then(() => this.props.history.push('/'));
+            signup(userInfo)
+                .then(() => this.props.history.push('/board'));
         }
     };
 
@@ -57,8 +58,13 @@ class _LoginPage extends React.Component {
         this.setState({ isLogin: !isLogin });
     };
 
+    responseGoogle = (response) => {
+        console.log(response);
+    };
+
     render() {
         const { isLogin } = this.state;
+        console.log(this.state);
         return (
 
             <section className='login-page'>
@@ -80,49 +86,54 @@ class _LoginPage extends React.Component {
                     <form >
                         <div className="inputs-container flex column">
                             {!isLogin &&
+                                <label> Full name
+                                    <input
+                                        className="input-style"
+                                        name="fullname"
+                                        id="fullName"
+                                        label="Full Name"
+                                        autoFocus
+                                        onChange={this.handleChange}
+
+                                    />
+                                </label>}
+
+                            <label> User name
                                 <input
                                     className="input-style"
-                                    name="fullname"
-
-                                    fullWidth
-                                    id="fullName"
-                                    label="Full Name"
-                                    autoFocus
+                                    label="User name"
+                                    name="username"
+                                    autoComplete="username"
                                     onChange={this.handleChange}
-
-                                />}
-
-                            <input
-                                className="input-style"
-                                label="User name"
-                                name="username"
-                                autoComplete="username"
-                                onChange={this.handleChange}
-
-                            />
-
-                            <input
-                                className="input-style"
-                                required
-
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                // autoComplete="current-password"
-                                onChange={this.handleChange}
-
-                            />
+                                />
+                            </label>
+                            <label> Password
+                                <input
+                                    className="input-style"
+                                    required
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    onChange={this.handleChange}
+                                />
+                            </label>
                             <button
                                 className="btn-style2"
                                 type="submit"
-
                                 onClick={this.onSubmit}
                             >
                                 {(isLogin) ? 'Login' : 'Sign-up'}
                             </button>
                         </div>
                     </form>
+                    <GoogleLogin
+                        clientId="1075713010675-m4s5vqqfj2kdl5t43hpfcao569uq0c4o.apps.googleusercontent.com"
+                        buttonText="Log-in with Google"
+                        onSuccess={this.onSubmit}
+                        onFailure={this.responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
                     <button className="toggle-login" onClick={() => this.toggleForm()}>
                         {(isLogin) ? `Don\'t have an account? Sign-up` : `Already have
                         an account? Login`}
@@ -136,6 +147,7 @@ class _LoginPage extends React.Component {
                 <div className="left-svg">
                     <img src={LoginSvgLeft} alt="LoginSvgLeft" />
                 </div>
+
             </section>
 
         );
@@ -145,7 +157,8 @@ class _LoginPage extends React.Component {
 
 
 const mapDispatchToProps = {
-
+    login,
+    logout,
 
 };
 
