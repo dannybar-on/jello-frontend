6920b2c2bbf7bcc486df1987c847befa1cc2ac3c
dannybar-on <93839751+dannybar-on@users.noreply.github.com>
2022-03-06 13:09:07 +0200

    home-page responsiveness fixes

--- src/cmps/Hero.jsx
+++ src/cmps/Hero.jsx
@@ -1,30 +1,25 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 import hero from '../assets/img/hero.png';
 
 export function Hero() {
+  return (
+    <React.Fragment>
+      <div className="hero-container flex align-center home-layout">
+        <div className="hero-text flex column">
+          <h1 className="hero-title">Jello helps teams move work forward.</h1>
 
-
-
-    return (
-
-        <React.Fragment>
-            <div className="hero-container flex align-center home-layout">
-                <div className="hero-text flex column" >
-                    <h1 className="hero-title">Jello helps teams move work forward.</h1>
-
-                    <p>Collaborate, manage projects, and reach new productivity
-                        peaks. From high rises to the home office,
-                        the way your team works is unique 
-                        accomplish it all with Jello.</p>
-                        <Link className="get-started-btn clean-link" to="/board/" >Try demo</Link>
-                </div>
-                <img src={hero}></img>
-
-            </div>
-        </React.Fragment>
-
-    );
-
-
+          <p>
+            Collaborate, manage projects, and reach new productivity peaks. From
+            high rises to the home office, the way your team works is unique
+            accomplish it all with Jello.
+          </p>
+          <Link className="get-started-btn clean-link" to="/board/">
+            Try demo
+          </Link>
+        </div>
+        <img className="hero-img" src={hero}></img>
+      </div>
+    </React.Fragment>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,66 +8,68 @@ import { Hero } from '../cmps/Hero.jsx';
 import { Loader } from '../cmps/Loader';
 
 export class HomePage extends React.Component {
-
   state = {
     isPageScrolled: false,
-  }
+  };
 
   componentDidMount() {
-    const ElHomePage = document.querySelector('.home-page')
-    ElHomePage.addEventListener('scroll', this.changeHeaderStyle)
-    ElHomePage.style.overflowY = 'scroll'
+    const ElHomePage = document.querySelector('.home-page');
+    ElHomePage.addEventListener('scroll', this.changeHeaderStyle);
+    ElHomePage.style.overflowY = 'scroll';
   }
 
   componentWillUnmount() {
-    const ElHomePage = document.querySelector('.home-page')
-    ElHomePage.removeEventListener('scroll', this.changeHeaderStyle)
-    ElHomePage.style.overflowY = 'hidden'
+    const ElHomePage = document.querySelector('.home-page');
+    ElHomePage.removeEventListener('scroll', this.changeHeaderStyle);
+    ElHomePage.style.overflowY = 'hidden';
   }
 
   changeHeaderStyle = () => {
-    const ElHomePage = document.querySelector('.home-page')
+    const ElHomePage = document.querySelector('.home-page');
 
-    const { isPageScrolled } = this.state
+    const { isPageScrolled } = this.state;
     if (ElHomePage.scrollTop > 50 && !isPageScrolled) {
-      this.setState({ isPageScrolled: true })
+      this.setState({ isPageScrolled: true });
     } else if (ElHomePage.scrollTop < 50 && isPageScrolled) {
-      this.setState({ isPageScrolled: false })
+      this.setState({ isPageScrolled: false });
     }
-  }
-
+  };
 
   render() {
-
-    const { isPageScrolled } = this.state
+    const { isPageScrolled } = this.state;
 
     return (
       <section className="home-page">
-        <header className={`home-header ${(isPageScrolled) ? 'bg-visible' : ''}`}>
+        <header className={`home-header ${isPageScrolled ? 'bg-visible' : ''}`}>
           <div className=" logo-container flex align-center">
-            <img className='jello wobble-top-on-hover' src={jello} />
-            <span className="logo">  Jello</span>
+            <img className="jello wobble-top-on-hover" src={jello} />
+            <span className="logo"> Jello</span>
           </div>
           <div className="header-buttons">
-            <Link to="board/login"><button className="login-btn nav-button">Log in</button></Link>
-            <Link to="board/login"><button className="signup-btn nav-button">Sign up</button></Link>
+            <Link to="board/login">
+              <button className="login-btn nav-button">Log in</button>
+            </Link>
+            <Link to="board/login">
+              <button className="signup-btn nav-button">Sign up</button>
+            </Link>
           </div>
         </header>
         <Hero />
         <div className="product flex column home-layout">
-          <h2 className="product-title">It’s more than work. It’s a way of working together.</h2>
+          <h2 className="product-title">
+            It’s more than work. It’s a way of working together.
+          </h2>
           <p>
-            Start with a Jello board, lists, and cards.
-            Customize and expand with more features as your teamwork grows.
-            Manage projects, organize tasks,
-            and build team spirit—all in one place.
+            Start with a Jello board, lists, and cards. Customize and expand
+            with more features as your teamwork grows. Manage projects, organize
+            tasks, and build team spirit—all in one place.
           </p>
-          <Link className="clean-link login-link" to="/board/login">Sign up-it's free!</Link>
+          <Link className="clean-link login-link" to="/board/login">
+            Sign up-it's free!
+          </Link>
           <img className="board-img" src={board} alt="" />
         </div>
       </section>
     );
   }
 }
-
-
